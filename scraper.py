b49d258d889ed4c2c5f3577f796ae6516be9e192
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-12-14 16:30:08 +0400

    url changed

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 ﻿from bs4 import BeautifulSoup
 import requests
-#import lxml
+import lxml
 
-url = 'https://www.armblog.net/'
+url = 'https://www.sas.am/food/search/?SORTBY=RELEVANSE&q=hac'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-quotes = soup.find_all("h2", class_="post-title entry-title")
+quotes = soup.find_all("div", class_="product__name hidden-sm")
+prices = soup.find_all("span", class_="price__text")
 titles = ""
+idx = 0
 for title in quotes:
-    titles = titles + title.text + "<br><hr>"
+    titles = titles + title.text + " >> " + prices[idx].text + "<br><hr>"
+    idx=idx+1
